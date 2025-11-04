d75e7af0ecdffe8e7d9a74ae694065751371fbc9
Przemyslaw Pacek <ppacek@op.pl>
2025-11-04 21:29:13 +0100

    changed App.js

---
+++ src/core/App.js
@@ -0,0 +1,13 @@
+import Form from "../Form";
+import { GlobalStyle } from "./GlobalStyle";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Form />
+    </>
+  );
+}
+
+export default App;
